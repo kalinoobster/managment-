b5be3af9fda12b6736f1177889e6718ef4bb5382
Kali Hehe <kalinoobster@gmail.com>
2025-08-01 13:10:45 +0000

    this is how it looks like

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,9 +7,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-      <div className="grid min-h-screen w-full md:grid-cols-[80px_1fr]">
+      <div className="grid min-h-screen w-full" style={{ gridTemplateColumns: '80px 1fr' }}>
         <Sidebar />
-        <div className="flex flex-col md:pl-20">
+        <div className="flex flex-col">
           <Header />
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
             {children}
